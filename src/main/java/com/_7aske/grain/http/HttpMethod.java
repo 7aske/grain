d0d87330225a11fb2f741ad18a74728db783ea5e
Nikola Tasic <ntasic7@gmail.com>
2021-08-29 18:56:16 +0200

    added http enums and constants

---
+++ src/main/java/com/_7aske/grain/http/HttpMethod.java
@@ -0,0 +1,28 @@
+package com._7aske.grain.http;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum HttpMethod {
+
+	GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
+
+	private static final Map<String, HttpMethod> mappings = new HashMap<>(16);
+
+	static {
+		for (HttpMethod httpMethod : values()) {
+			mappings.put(httpMethod.name(), httpMethod);
+		}
+	}
+
+
+	public static HttpMethod resolve(String method) {
+		return (method != null ? mappings.get(method) : null);
+	}
+
+
+	public boolean matches(String method) {
+		return name().equals(method);
+	}
+
+}
